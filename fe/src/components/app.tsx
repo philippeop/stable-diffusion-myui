38846e1de625fcd4f6213bcba2a61a8614b97d15
philippeop <pp.89@live.com>
2023-05-24 15:28:28 -0400

    collpase loras/embeds

--- fe/src/components/app.tsx
+++ fe/src/components/app.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState, useTransition } from 'react';
 import { Logger } from '@common/logger';
 import Options from './options'
 import Gallery from './gallery';
@@ -8,11 +8,13 @@ import Actions from './actions';
 import { SdApi } from '@/services/sdapi.service';
 import { Embedding, Lora } from '@/common/models/sdapi.models';
 
-import config from '../../config.json' 
+import config from '../../config.json'
+import classNames from 'classnames';
 
 export default function App() {
     Logger.debug('Rendering App')
 
+    const [ showLoras, setShowLoras ] = useState<boolean>()
     const [loras, setLoras] = useState<Lora[]>([])
     const [embeddings, setEmbeddings] = useState<Embedding[]>([])
 
@@ -25,17 +27,25 @@ export default function App() {
         })()
     }, [setLoras])
 
+    const toggleLoras = useCallback(() => {
+        setShowLoras(!showLoras)
+    }, [loras, showLoras])
+
     const loraElements = loras.map(l => {
         const label = l.name === l.alias ? l.name : `${l.name} (alias: ${l.alias})`
         const loraDef = config.lora_triggers.find(t => t.name === l.name)
         let title = ''
-        if(loraDef) {
+        if (loraDef) {
             title = (loraDef.comment ? (loraDef.comment + '\n') : '') + loraDef.keywords.join(', ')
         }
         return <li key={l.name} title={title}>{label}</li>
     })
     const embedElements = embeddings.map(e => {
         return <li key={e.name}>{e.name}{e.skipped && ' (skipped)'}</li>
+    })
+
+    const loraClass = classNames({
+        'hidden': !showLoras
     })
 
     return (
@@ -47,16 +57,20 @@ export default function App() {
             </div>
             <div className='row'>
                 <div>
-                    Loras:
-                    <ul>
-                        {loraElements}
-                    </ul>
+                    <div onClick={() => toggleLoras()}>Loras:</div>
+                    <div className={loraClass}>
+                        <ul>
+                            {loraElements}
+                        </ul>
+                    </div>
                 </div>
                 <div>
-                    Embeddings:
-                    <ul>
-                        {embedElements}
-                    </ul>
+                    <div onClick={() => toggleLoras()}>Embeddings:</div>
+                    <div className={loraClass}>
+                        <ul>
+                            {embedElements}
+                        </ul>
+                    </div>
                 </div>
             </div>
             <Gallery />
